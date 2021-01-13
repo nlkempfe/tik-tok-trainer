ded7c22d51869c7d2c19574bc3907efac6b29f76
Hunter Jarrell <hjarrell555@gmail.com>
2021-01-13 17:02:19 -0500

    Initial Commit

---
+++ TikTokTrainer/TikTokTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokTrainerApp.swift
+//  TikTokTrainer
+//
+//  Created by Hunter Jarrell on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct TikTokTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
