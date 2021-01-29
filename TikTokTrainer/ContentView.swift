75de143edecc45b115071ef475d807f1c6791614
David Sadowsky <41664274+DavidSadowsky@users.noreply.github.com>
2021-01-29 17:16:22 -0500

    Save Video (#7)
    * Save video
    
    * Fix blocking call from no camera
    
    * Linting
    
    * More linting :(
    
    * Fix flashlight
    
    * break up code into multiple files
    
    * Lint
    
    * Fix multiple recordings bug
    
    Co-authored-by: David Sadowsky <davidsadowsky@Davids-MacBook-Pro.local>

--- TikTokTrainer/ContentView.swift
+++ TikTokTrainer/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import AVFoundation
+import Photos
 
 struct ContentView: View {
     var body: some View {
@@ -22,14 +23,13 @@ struct ContentView_Previews: PreviewProvider {
 
 struct CameraView: View {
     @StateObject var camera = CameraModel()
-    @StateObject var flashlight = FlashlightModel()
     var body: some View {
         ZStack {
-            
+
             // Going to be camera preview
             CameraPreview(camera: camera)
                 .ignoresSafeArea(.all, edges: .all)
-            
+
             VStack {
                     HStack {
                         Spacer()
@@ -43,25 +43,26 @@ struct CameraView: View {
                                 })
                                 .scaleEffect(CGSize(width: 1.5, height: 1.5))
                                 .padding(.trailing, 10)
-                                if !flashlight.isOn {
-                                    Button(action: {flashlight.isOn.toggle()}, label: {
-                                        Image(systemName: "bolt")
-                                            .foregroundColor(.white)
-                                            .padding()
-                                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                                        })
-                                        .scaleEffect(CGSize(width: 1.5, height: 1.5))
-                                        .padding(.trailing, 10)
+                                if camera.backCameraOn {
+                                    if !camera.flashlightOn {
+                                        Button(action: {camera.toggleFlash()}, label: {
+                                            Image(systemName: "bolt")
+                                                .foregroundColor(.white)
+                                                .padding()
+                                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                            })
+                                            .scaleEffect(CGSize(width: 1.5, height: 1.5))
+                                            .padding(.trailing, 10)
+                                        } else {
+                                            Button(action: {camera.toggleFlash()}, label: {
+                                            Image(systemName: "bolt.fill")
+                                                .foregroundColor(.white)
+                                                .padding()
+                                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                            })
+                                            .scaleEffect(CGSize(width: 1.5, height: 1.5))
+                                            .padding(.trailing, 10)
                                     }
-                                else {
-                                    Button(action: {flashlight.isOn.toggle()}, label: {
-                                        Image(systemName: "bolt.fill")
-                                            .foregroundColor(.white)
-                                            .padding()
-                                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                                        })
-                                        .scaleEffect(CGSize(width: 1.5, height: 1.5))
-                                        .padding(.trailing, 10)
                                 }
                             }
                         }
@@ -69,7 +70,7 @@ struct CameraView: View {
                 Spacer()
                 HStack {
                     if camera.isTaken {
-                        Button(action: {camera.stopRecord(flashlight: flashlight)}, label: {
+                        Button(action: {camera.stopRecord()}, label: {
                             ZStack {
                                 Circle()
                                     .fill(Color.red)
@@ -79,9 +80,8 @@ struct CameraView: View {
                                     .frame(width: 75, height: 75)
                             }
                         })
-                    }
-                    else {
-                        Button(action: {camera.startRecord(flashlight: flashlight)}, label: {
+                    } else {
+                        Button(action: {camera.startRecord()}, label: {
                             ZStack {
                                 Circle()
                                     .stroke(Color.white, lineWidth: 2)
@@ -94,195 +94,7 @@ struct CameraView: View {
             }
         }
         .onAppear(perform: {
-            camera.Check()
+            camera.check()
         })
-    }
-}
-
-class CameraModel: NSObject, ObservableObject {
-    @Published var isTaken = false
-    @Published var session = AVCaptureSession()
-    @Published var alert = false
-    @Published var backCameraOn = true
-    @Published var output = AVCaptureVideoDataOutput()
-    @Published var preview : AVCaptureVideoPreviewLayer!
-    @Published var backCamera : AVCaptureDevice!
-    @Published var frontCamera : AVCaptureDevice!
-    @Published var backInput : AVCaptureInput!
-    @Published var frontInput : AVCaptureInput!
-    
-    func Check() {
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .authorized:
-            setUp()
-            return
-        case .notDetermined:
-            AVCaptureDevice.requestAccess(for: .video) { (status) in
-                if status {
-                    self.setUp()
-                }
-            }
-        case .denied:
-            self.alert.toggle()
-            return
-        default:
-            return
-        }
-    }
-    
-    func setUp() {
-        //start configuration
-        self.session.beginConfiguration()
-                    
-        //session specific configuration
-        if self.session.canSetSessionPreset(.photo) {
-            self.session.sessionPreset = .photo
-        }
-        self.session.automaticallyConfiguresCaptureDeviceForWideColor = true
-                    
-        //setup inputs
-        //get back camera
-        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
-            self.backCamera = device
-        } else {
-            //handle this appropriately for production purposes
-            fatalError("no back camera")
-        }
-                    
-        //get front camera
-        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) {
-            self.frontCamera = device
-        } else {
-            fatalError("no front camera")
-        }
-    
-        guard let bInput = try? AVCaptureDeviceInput(device: backCamera) else {
-            fatalError("could not create input device from back camera")
-        }
-        backInput = bInput
-        if !session.canAddInput(backInput) {
-            fatalError("could not add back camera input to capture session")
-        }
-                    
-        guard let fInput = try? AVCaptureDeviceInput(device: frontCamera) else {
-            fatalError("could not create input device from front camera")
-        }
-        frontInput = fInput
-        if !session.canAddInput(frontInput) {
-            fatalError("could not add front camera input to capture session")
-        }
-        
-        self.session.addInput(backInput)
-            
-        //setup outputs
-        self.session.addOutput(self.output)
-                    
-        //commit configuration
-        self.session.commitConfiguration()
-
-    }
-    
-    func setupOutputs(){
-        let videoQueue = DispatchQueue(label: "videoQueue", qos: .userInteractive)
-        output.setSampleBufferDelegate(self, queue: videoQueue)
-            
-        if session.canAddOutput(output) {
-            session.addOutput(output)
-        } else {
-            fatalError("could not add video output")
-        }
-        //deal with the orientation
-        output.connections.first?.videoOrientation = .portrait
-    }
-    
-    func startRecord(flashlight: FlashlightModel) {
-        self.isTaken.toggle()
-        if(flashlight.isOn) {
-            flashlight.toggleFlash()
-        }
-    }
-    
-    func stopRecord(flashlight: FlashlightModel) {
-        self.isTaken.toggle()
-        if(flashlight.isOn) {
-            flashlight.toggleFlash()
-        }
-        
-    }
-    
-    func switchCameraInput(){
-           //don't let user spam the button, fun for the user, not fun for performance
-           
-           //reconfigure the input
-           session.beginConfiguration()
-           if backCameraOn {
-                session.removeInput(self.backInput)
-                session.addInput(self.frontInput)
-                backCameraOn = false
-           } else {
-                session.removeInput(self.frontInput)
-                session.addInput(self.backInput)
-                backCameraOn = true
-           }
-           
-           //deal with the connection again for portrait mode
-           output.connections.first?.videoOrientation = .portrait
-           
-           //commit config
-           session.commitConfiguration()
-       }
-}
-
-extension CameraModel: AVCaptureVideoDataOutputSampleBufferDelegate {
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        
-    }
-}
-
-struct CameraPreview: UIViewRepresentable {
-    @ObservedObject var camera : CameraModel
-    
-    func makeUIView(context: Context) -> some UIView {
-        let view = UIView(frame: UIScreen.main.bounds)
-        
-        camera.preview = AVCaptureVideoPreviewLayer(session: camera.session)
-        camera.preview.frame = view.frame
-        
-        camera.preview.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(camera.preview)
-        
-        camera.session.startRunning()
-        return view
-    }
-    
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        
-    }
-}
-
-class FlashlightModel: ObservableObject {
-    @Published var isOn = false
-    
-    func toggleFlash() {
-        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
-        guard device.hasTorch else { return }
-
-        do {
-            try device.lockForConfiguration()
-
-            if (device.torchMode == AVCaptureDevice.TorchMode.on) {
-                device.torchMode = AVCaptureDevice.TorchMode.off
-            } else {
-                do {
-                    try device.setTorchModeOn(level: 1.0)
-                } catch {
-                    print(error)
-                }
-            }
-
-            device.unlockForConfiguration()
-        } catch {
-            print(error)
-        }
     }
 }
