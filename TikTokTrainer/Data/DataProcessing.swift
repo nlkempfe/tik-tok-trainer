4c596f6409ed8a5477869335f0d3daea7cfb235c
griffin <west.griffin@ufl.edu>
2021-02-19 12:10:50 -0500

    angles workin

--- TikTokTrainer/Data/DataProcessing.swift
+++ TikTokTrainer/Data/DataProcessing.swift
@@ -23,50 +23,34 @@ struct ScoringFunction {
         let leftShoulder = result.points[.leftShoulder]!
         let leftElbow = result.points[.leftElbow]!
         let leftWrist = result.points[.leftWrist]!
+        let neck = result.points[.neck]!
+        let nose = result.points[.nose]!
         
-        let leftArmAngle: CGFloat = ((atan2(leftWrist.y - leftElbow.y, leftWrist.x - leftElbow.x)) - (atan2(leftElbow.y - leftShoulder.y, leftElbow.x - leftShoulder.x))) * (180 / .pi)
+        var angle = angleBetweenPoints(leftPoint: leftWrist, middlePoint: leftElbow, rightPoint: leftShoulder)
+        print("leftElbow: " + angle.description)
+        angle = angleBetweenPoints(leftPoint: leftElbow, middlePoint: leftShoulder, rightPoint: neck)
+        print("leftShoulder: " + angle.description)
+        angle = angleBetweenPoints(leftPoint: nose, middlePoint: neck, rightPoint: leftShoulder)
+        print("leftNeck: " + angle.description)
+        print()
         
-        print(leftArmAngle)
-        return leftArmAngle
-        
-//        do {
-//                    for slice in try result.get().data {
-//                        print(String(slice.start.value) + ":")
-//                        let leftElbow = angleBetweenPoints(leftPoint: slice.points[VNRecognizedPointKey(rawValue: "left_shoulder_1_joint")]!, middlePoint: slice.points[VNRecognizedPointKey(rawValue: "left_forearm_joint")]!, rightPoint: slice.points[VNRecognizedPointKey(rawValue: "left_hand_joint")]!)
-//                        print("     left elbow: " + String(leftElbow))
-//                        let leftShoulder = angleBetweenPoints(leftPoint: slice.points[VNRecognizedPointKey(rawValue: "neck_1_joint")]!, middlePoint: slice.points[VNRecognizedPointKey(rawValue: "left_shoulder_1_joint")]!, rightPoint: slice.points[VNRecognizedPointKey(rawValue: "left_forearm_joint")]!)
-//                        print("     left shoulder: " + String(leftShoulder))
-//                        let leftNeck = angleBetweenPoints(leftPoint: slice.points[VNRecognizedPointKey(rawValue: "left_shoulder_1_joint")]!, middlePoint: slice.points[VNRecognizedPointKey(rawValue: "neck_1_joint")]!, rightPoint: slice.points[VNRecognizedPointKey(rawValue: "head_joint")]!)
-//                        print("     left neck: " + String(leftNeck))
-//                        let rightNeck = angleBetweenPoints(leftPoint: slice.points[VNRecognizedPointKey(rawValue: "head_joint")]!, middlePoint: slice.points[VNRecognizedPointKey(rawValue: "neck_1_joint")]!, rightPoint: slice.points[VNRecognizedPointKey(rawValue: "right_shoulder_1_joint")]!)
-//                        print("     right neck: " + String(rightNeck))
-//                        let rightShoulder = angleBetweenPoints(leftPoint: slice.points[VNRecognizedPointKey(rawValue: "right_forearm_joint")]!, middlePoint: slice.points[VNRecognizedPointKey(rawValue: "right_shoulder_1_joint")]!, rightPoint: slice.points[VNRecognizedPointKey(rawValue: "neck_1_joint")]!)
-//                        print("     right shoulder: " + String(rightShoulder))
-//                        let rightElbow = angleBetweenPoints(leftPoint: slice.points[VNRecognizedPointKey(rawValue: "right_hand_joint")]!, middlePoint: slice.points[VNRecognizedPointKey(rawValue: "right_forearm_joint")]!, rightPoint: slice.points[VNRecognizedPointKey(rawValue: "right_shoulder_1_joint")]!)
-//                        print("     right elbow: " + String(rightElbow))
-//
-//                    }
-//                } catch {
-//                    print(error)
-//                }
-//            }
-//
-//            static func angleBetweenPoints(leftPoint: VNRecognizedPoint, middlePoint: VNRecognizedPoint, rightPoint: VNRecognizedPoint) -> Float64 {
-//                print("         right point: " + String(rightPoint.x) + ", " + String(rightPoint.y))
-//                print("         middle point: " + String(middlePoint.x) + ", " + String(middlePoint.y))
-//                print("         left vector: " + String(leftPoint.x) + ", " + String(leftPoint.y))
-//                let rightVector = (x: rightPoint.x - middlePoint.x, y: rightPoint.y - middlePoint.y)
-//                let leftVector = (x: leftPoint.x - middlePoint.x, y: leftPoint.y - middlePoint.y)
-//                print("         right vector: " + String(rightVector.x) + ", " + String(rightVector.y))
-//                print("         left vector: " + String(leftVector.x) + ", " + String(leftVector.y))
-//                let dotProduct = rightVector.x * leftVector.x + rightVector.y * leftVector.y
-//                let determinant = rightVector.x * leftVector.y - rightVector.y * leftVector.x
-//                var angle = atan2(determinant, dotProduct)
-//                if angle < 0 {
-//                    angle = angle + 360
-//                }
-//                return angle
-//            }
+        return angle
+    }
+    
+    func angleBetweenPoints(leftPoint: VNRecognizedPoint, middlePoint: VNRecognizedPoint, rightPoint: VNRecognizedPoint) -> CGFloat {
+        return angleBetweenPoints(leftPoint: CGPoint(x: leftPoint.x, y: leftPoint.y), middlePoint: CGPoint(x: middlePoint.x, y: middlePoint.y), rightPoint: CGPoint(x: rightPoint.x, y: rightPoint.y))
+    }
+    
+    func angleBetweenPoints(leftPoint: CGPoint, middlePoint: CGPoint, rightPoint: CGPoint) -> CGFloat {
+        let rightVector = (x: rightPoint.x - middlePoint.x, y: rightPoint.y - middlePoint.y)
+        let leftVector = (x: leftPoint.x - middlePoint.x, y: leftPoint.y - middlePoint.y)
+        let dotProduct = rightVector.x * leftVector.x + rightVector.y * leftVector.y
+        let determinant = rightVector.x * leftVector.y - rightVector.y * leftVector.x
+        var angle = atan2(determinant, dotProduct) * (180 / .pi)
+        if angle < 0 {
+            angle = angle + 360
+        }
+        return angle
     }
     
 }
