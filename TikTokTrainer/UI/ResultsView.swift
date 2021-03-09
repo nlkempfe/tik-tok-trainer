402a40582a74653266e03805b980065a2d0cd4d4
Natascha Kempfe <nlkempfe@gmail.com>
2021-03-09 00:04:27 -0500

    Create results screen
    - Add appropriate state variables for scoring and sheet display
    - Remove scoring and submit debugging print statements
    - Hide submit button during loading screen
    - Rearrange sheets
    - Create ResultsView
    
    TODO:
    - Remove debugging print statements and add functionality for submit method
    - Resize LoopingPlayer (?)
    - Text formatting and replacing dummy variables

---
+++ TikTokTrainer/UI/ResultsView.swift
@@ -0,0 +1,47 @@
+//
+//  ResultsView.swift
+//  TikTokTrainer
+//
+//  Created by Natascha Kempfe on 3/8/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct ResultsView: View {
+    var score: CGFloat
+    var duration: Double
+    var url: URL
+    var playbackRate: Double
+    
+    var submitButton: some View {
+        Button(action: {
+            print("submit button pressed")
+        }, label: {
+            Text("Submit")
+                .foregroundColor(.white)
+                .padding(.leading, 20)
+                .padding(.trailing, 20)
+                .padding(.top, 10)
+                .padding(.bottom, 10)
+        })
+        .background(Color.blue)
+        .cornerRadius(15)
+    }
+    
+    var body: some View {
+        VStack {
+            LoopingPlayer(url: self.url, playbackRate: self.playbackRate, isUploadedVideo: false)
+            Spacer()
+            Text("Score: \(score)%")
+                .padding(.bottom, 10)
+            Text("Mistakes: MISTAKES")
+                .padding(.bottom, 10)
+            Text("Duration: \(Int(round(duration))) seconds")
+            Spacer()
+            submitButton
+        }
+        .background(Color.white)
+    }
+}
+
