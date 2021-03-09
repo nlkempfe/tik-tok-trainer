d46ee4f559dc3a74bdd0335d15ba2ecce30d42ff
Ank <a.vishnubhotla@ufl.edu>
2021-03-09 14:22:34 -0500

    rotational error added

--- TikTokTrainer/TikTokTrainerApp.swift
+++ TikTokTrainer/TikTokTrainerApp.swift
@@ -15,17 +15,4 @@ struct TikTokTrainerApp: App {
             ContentView()
         }
     }
-    
-    init() {
-        all(
-            PoseNetProcessor.run(url: Bundle.main.url(forResource: "TrueDance", withExtension: "mov")!),
-            PoseNetProcessor.run(url: Bundle.main.url(forResource: "TestDanceTrimmed", withExtension: "mov")!)
-        ).then { movieOne, movieTwo in
-            ScoringFunction(preRecordedVid: movieOne, recordedVid: movieTwo).computeScore()
-        }.then { score in
-            print(score)
-        }.catch { error in
-            print("Error: \(error)")
-        }
-    }
 }
