e5d8530abd11bd53a03e2b909891dd0e2d3123ac
Natascha Kempfe <nlkempfe@gmail.com>
2021-03-23 15:23:23 -0400

    Linted.

--- TikTokTrainer/Data/DataProcessing.swift
+++ TikTokTrainer/Data/DataProcessing.swift
@@ -128,19 +128,19 @@ struct ScoringFunction {
 
         return prevDistance == 0 ? 1 : percentChange
     }
-      
+
     private func CGPointDistance(from: CGPoint, to: CGPoint) -> CGFloat {
         return sqrt((from.x - to.x) * (from.x - to.x) + (from.y - to.y) * (from.y - to.y))
     }
-  
+
     private func computeAngleDifferences(preRecordedVid: ProcessedVideo, recordedVid: ProcessedVideo) -> [[String: CGFloat]] {
 
         let preRecordedPoses = computeAngles(video: preRecordedVid)
         let recordedPoses = computeAngles(video: recordedVid)
         let minSlices = min(preRecordedVid.data.count, recordedVid.data.count)
-        
+
         var angleDifferences = [[String: CGFloat]]()
-    
+
         for (row, poseAngles) in preRecordedPoses.enumerated() where row < minSlices {
             let slicesToCheck = 3
             // this also works well, need bigger sample size
@@ -158,7 +158,7 @@ struct ScoringFunction {
             }
             angleDifferences.append(sliceData)
         }
-        
+
         return angleDifferences
     }
 
@@ -243,7 +243,7 @@ struct ScoringFunction {
                 tempSum += pow(angle.value, 2)
             }
             error += sqrt(tempSum)
-            if (tempSum < lowError) {
+            if tempSum < lowError {
                 lowError = tempSum
             }
             tempSum = 0
