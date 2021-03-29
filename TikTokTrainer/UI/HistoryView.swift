daa292778e9bbcfd4c399778907c3a620234deb2
Natascha Kempfe <nlkempfe@gmail.com>
2021-03-29 15:12:54 -0400

    Update HistoryView.swift
    - Add title bar and select, delete, and cancel buttons
    - Add edit capabilities to list component
    - Implement entity deletion for StoredResult

--- TikTokTrainer/UI/HistoryView.swift
+++ TikTokTrainer/UI/HistoryView.swift
@@ -16,13 +16,81 @@ struct HistoryView: View {
             NSSortDescriptor(keyPath: \StoredResult.timestamp, ascending: true)
         ]
     ) var results: FetchedResults<StoredResult>
+    @State var editMode = EditMode.inactive
+    @State var selection = Set<StoredResult>()
+
+    var background: some View {
+        Rectangle()
+            .fill()
+            .ignoresSafeArea(.all)
+            .background(Color.white)
+            .foregroundColor(Color.white)
+    }
+    
+    var deleteButton: some View {
+        if editMode == .inactive {
+            return Button(action: {}) {
+                Image(systemName: "")
+            }
+        } else {
+            return Button(action: deleteResults) {
+                Image(systemName: "trash")
+            }
+        }
+    }
+    
+    var selectButton: some View {
+        if editMode == .inactive {
+            return Button(action: {
+                self.editMode = .active
+                self.selection = Set<StoredResult>()
+            }) {
+                Text("Select")
+                    .foregroundColor(Color.black)
+            }
+        }
+        else {
+            return Button(action: {
+                self.editMode = .inactive
+                self.selection = Set<StoredResult>()
+            }) {
+                Text("Cancel")
+                    .foregroundColor(Color.black)
+            }
+        }
+    }
+    
+    private func deleteResults() {
+        self.editMode = .inactive
+        for result in selection {
+            managedObjectContext.delete(result)
+        }
+        DataController.shared.save()
+        selection = Set<StoredResult>()
+    }
 
     var body: some View {
-//        List(videos, id: \.self) { (video: StoredVideo) in
-//            Text(video.location?.absoluteString ?? "not found")
-//        }
-        List(results, id: \.self) { (result: StoredResult) in
-            Text("Video")
+        ZStack {
+            background
+            VStack {
+                HStack {
+                    deleteButton
+                        .padding(.leading, 20)
+                    Spacer()
+                    selectButton
+                        .padding(.trailing, 10)
+                }
+                Text("Results")
+                    .font(.largeTitle)
+                List(selection: $selection) {
+                    ForEach(results, id: \.self) { (result: StoredResult) in
+                        Text("Resulted: ") + Text(result.timestamp ?? Date.init(), style: .date) + Text(" ") + Text(result.timestamp ?? Date.init(), style: .time)
+                            .foregroundColor(Color.black)
+                    }
+                }
+                .environment(\.editMode, self.$editMode)
+            }
+            .background(Color.white)
         }
     }
 }
